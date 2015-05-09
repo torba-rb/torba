8af67df52808f5358a8c0902c89cb1160ae5bda2
Nash Bridges <pleasesendmeaspam@gmail.com>
2015-05-09 22:48:36 +0300

    Add zip package to acceptance test

--- test/acceptance_test.rb
+++ test/acceptance_test.rb
@@ -6,19 +6,36 @@ module Torba
       @manifest ||= Manifest.build("test/Torbafile")
     end
 
+    def find(file_path)
+      manifest.load_path.map{ |lp| File.join(lp, file_path) }.find{ |full_path| File.exists?(full_path) }
+    end
+
     def assert_exists(file_path)
-      super File.join(manifest.load_path, file_path)
+      assert find(file_path)
+    end
+
+    def refute_exists(file_path)
+      refute find(file_path)
+    end
+
+    def read(file_path)
+      File.read(find(file_path))
     end
 
     def test_pack
       manifest.pack
+
+      assert_exists "trumbowyg-from-zip/trumbowyg.js"
+      refute_exists "trumbowyg-from-zip/trumbowyg.css.erb"
+      refute_exists "trumbowyg-from-zip/icons.png"
+      refute_exists "trumbowyg-from-zip/icons-2x.png"
 
       assert_exists "trumbowyg/trumbowyg.js"
       assert_exists "trumbowyg/trumbowyg.css.erb"
       assert_exists "trumbowyg/icons.png"
       assert_exists "trumbowyg/icons-2x.png"
 
-      css = File.read File.join(manifest.load_path, "trumbowyg/trumbowyg.css.erb")
+      css = read "trumbowyg/trumbowyg.css.erb"
       assert_includes css, "background: transparent url('<%= asset_path('trumbowyg/icons.png') %>') no-repeat;"
       assert_includes css, "background-image: url('<%= asset_path('trumbowyg/icons-2x.png') %>')"
     end
