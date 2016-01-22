f04206153782d29f494971c75ee068a878df7f11
Matt Brictson <matt@mattbrictson.com>
2016-01-21 17:43:25 -0800

    Don't verify during Rails Rake execution

--- lib/torba/verify.rb
+++ lib/torba/verify.rb
@@ -1,7 +1,14 @@
 require "torba"
 
-if STDOUT.tty?
-  Torba.pretty_errors { Torba.verify }
-else
-  Torba.verify
+# We purposely skip verification during Rake execution, because Rake is used to
+# run the Torba:RakeTask during the "assets:precompile" stage of deployment. In
+# this case it is OK that the Torbafile hasn't been packed yet, because we are
+# about to do so.
+#
+unless defined?(Rake::Task)
+  if STDOUT.tty?
+    Torba.pretty_errors { Torba.verify }
+  else
+    Torba.verify
+  end
 end
