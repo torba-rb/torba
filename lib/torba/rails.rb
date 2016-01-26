b190551c7bdaf109e80bf3bf1567d3461b3bf362
Lucas Mazza <lucastmazza@gmail.com>
2016-01-26 16:42:01 -0200

    Dot not pollute `precompile` config with all assets on Sprockets 4.
    Precompiled assets should be linked through the `app/assets/config/manifest.js`
    instead.

--- lib/torba/rails.rb
+++ lib/torba/rails.rb
@@ -2,7 +2,13 @@ module Torba
   class Engine < Rails::Engine
     initializer "torba.assets" do |app|
       Rails.application.config.assets.paths.concat(Torba.load_path)
-      Rails.application.config.assets.precompile.concat(Torba.non_js_css_logical_paths)
+      unless using_sprockets4?
+        Rails.application.config.assets.precompile.concat(Torba.non_js_css_logical_paths)
+      end
+    end
+
+    def using_sprockets4?
+      Gem::Version.new(Sprockets::VERSION) >= Gem::Version.new('4.x')
     end
 
     rake_tasks do
